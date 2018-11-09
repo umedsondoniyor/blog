74b2d035179b1247c8e3dc15d94d1322272a6e0c
umedsondoniyor <umedzhonizbasarov@gmail.com>
2018-11-10 00:34:19 +0300

    slugify is done

--- post/urls.py
+++ post/urls.py
@@ -6,12 +6,8 @@ app_name = 'post'
 urlpatterns = [
 
     url(r'^index/$', post_index, name='index'),
-
-    url(r'^(?P<id>\d+)/$', post_detail, name='detail'),      # d+ regular expressions means that it can include many digits
-
     url(r'^create/$', post_create, name='create'),
-
-    url(r'^(?P<id>\d+)/update/$', post_update, name='update'),
-
-    url(r'^(?P<id>\d+)/delete/$', post_delete, name='delete'),
+    url(r'^(?P<slug>[\w-]+)/$', post_detail, name='detail'),      # d+ regular expressions means that it can include many digits
+    url(r'^(?P<slug>[\w-]+)/update/$', post_update, name='update'),
+    url(r'^(?P<slug>[\w-]+)/delete/$', post_delete, name='delete'),
 ]
