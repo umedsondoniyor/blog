fd847e0ee7bced15cac24d47d79f7d0a71caa23c
umedsondoniyor <umedzhonizbasarov@gmail.com>
2018-11-11 00:18:04 +0300

    almost done

--- blog/urls.py
+++ blog/urls.py
@@ -23,11 +23,14 @@ from django.conf import  settings
 
 urlpatterns = [
 
+    url(r'^admin/', admin.site.urls),
+
     url(r'^$', home_view, name='home'),
 
     url(r'^post/', include('post.urls')),
 
-    url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('accounts.urls')),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
